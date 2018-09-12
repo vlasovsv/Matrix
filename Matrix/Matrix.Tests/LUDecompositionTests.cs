477ffc31345e881aa0485af2f1367e565de7a5cc
vlasovsv <vlasovsv@gmail.com>
2018-09-12 22:45:07 +0300

    Added LU decomposition. Added LU decomposition test.

---
+++ Matrix/Matrix.Tests/LUDecompositionTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+using System.Numerics.Algorithms;
+
+namespace System.Numerics
+{
+    [TestFixture()]
+    public class LUDecompositionTests
+    {
+        [Test]
+        public void LUDecomposition_GetLUMatrix_ReturnsAsExpected()
+        {
+            var luFactorization = new LUDecomposition();
+            var matrix = new Matrix(2, 2, new double[,] { { 4, 3 }, { 6, 3 } });
+
+            var expectedL = new Matrix(2, 2, new double[,] { { 1, 0 }, { 1.5, 1 } });
+            var expectedU = new Matrix(2, 2, new double[,] { { 4, 3 }, { 0, -1.5 } });
+
+
+            Matrix l;
+            Matrix u;
+            luFactorization.CalculateLUMatrices(matrix, out l, out u);
+
+            Assert.AreEqual(expectedL, l);
+            Assert.AreEqual(expectedU, u);
+        }
+    }
+}
---
+++ Matrix/Matrix/Algorithms/LUDecomposition.cs
@@ -0,0 +1,52 @@
+﻿namespace System.Numerics.Algorithms
+{
+    /// <summary>
+    /// LU-decomposition solver.
+    /// </summary>
+    public class LUDecomposition
+    {
+        #region Methods
+
+        /// <summary>
+        /// Calculates l (lower) and u (upper) matrices for current matrix.
+        /// </summary>
+        /// <param name="matrix">Current matrix.</param>
+        /// <param name="lower">Out l (lower) matrix.</param>
+        /// <param name="upper">Out u (upper) matrix.</param>
+        public void CalculateLUMatrices(Matrix matrix, out Matrix lower, out Matrix upper)
+        {
+            lower = new Matrix(matrix.Rows, matrix.Columns);
+            upper = new Matrix(matrix.Rows, matrix.Columns);
+
+            for (int j = 0; j < matrix.Columns; j++)
+            {
+                upper[0, j] = matrix[0, j];
+                lower[j, 0] = matrix[j, 0] / upper[0, 0];
+            }
+
+            for (int i = 1; i < matrix.Rows; i++)
+            {
+                for (int j = i; j < matrix.Columns; j++)
+                {
+                    double sum = 0;
+                    for (int k = 0; k < i; k++)
+                    {
+                        sum += lower[i, k] * upper[k, j];
+                    }
+                    upper[i, j] = matrix[i, j] - sum;
+
+                    sum = 0;
+
+                    for (int k = 0; k < i; k++)
+                    {
+                        sum += lower[j, k] * upper[k, i];
+                    }
+
+                    lower[j, i] = (1 / upper[i, i]) * (matrix[j, i] - sum);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
